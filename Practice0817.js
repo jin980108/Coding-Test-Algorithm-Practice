642dc692164bb8ae9b2d850ba1a2f8675d6476a0
gyeongjin <wodlstmdfl@naver.com>
2025-08-17 01:01:58 +0900

    0817 알고리즘 테스트

---
+++ Practice0817.js
@@ -0,0 +1,23 @@
+//수 조작하기1
+
+function solution(n, control) {
+    
+    for(let i=0;i<control.length;i++){
+        switch (control[i]) {
+            case 'w':
+                n += 1;
+                break
+            case 'a':
+                n -= 10;
+                break;
+            case 's':
+                n -= 1;
+                break;
+            case 'd':
+                n += 10;
+                break;
+        }
+    }
+    
+    return n;
+}
