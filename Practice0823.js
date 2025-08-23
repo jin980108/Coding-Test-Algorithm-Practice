9a891c4c1724fdad5565ae8f703127ec1af225f3
gyeongjin <wodlstmdfl@naver.com>
2025-08-23 20:24:02 +0900

    0823 알고리즘

---
+++ Practice0823.js
@@ -0,0 +1,14 @@
+//배열 만들기 2
+
+function solution(l, r) {
+    var answer = [];
+    for(let i=l;i<=r;i++){
+        let arr=String(i)
+        if(![...arr].every(x=>x==='5'||x==='0'))continue;
+        answer.push(i)
+        
+    }
+    return answer.length? answer:[-1];
+}
+
+console.log(solution(10,20))
